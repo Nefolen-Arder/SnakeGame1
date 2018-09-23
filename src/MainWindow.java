30260a9ef34e842cddca3b5acf753228f7925971
Nefolen-Arder <37435134+Nefolen-Arder@users.noreply.github.com>
2018-09-23 18:20:03 +0300

    Snake

---
+++ src/MainWindow.java
@@ -0,0 +1,28 @@
+/* ctrl+v полученной ошибки
+"C:\Program Files\Java\jdk1.8.0_181\bin\java.exe" "-javaagent:D:\Илья работы\programs\IntelliJ IDEA 2018.2.4\lib\idea_rt.jar=51512:D:\Илья работы\programs\IntelliJ IDEA 2018.2.4\bin" -Dfile.encoding=UTF-8 -classpath "C:\Program Files\Java\jdk1.8.0_181\jre\lib\charsets.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\deploy.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\ext\access-bridge-64.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\ext\cldrdata.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\ext\dnsns.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\ext\jaccess.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\ext\jfxrt.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\ext\localedata.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\ext\nashorn.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\ext\sunec.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\ext\sunjce_provider.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\ext\sunmscapi.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\ext\sunpkcs11.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\ext\zipfs.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\javaws.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\jce.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\jfr.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\jfxswt.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\jsse.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\management-agent.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\plugin.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\resources.jar;C:\Program Files\Java\jdk1.8.0_181\jre\lib\rt.jar;D:\Илья работы\program\out\production\Snakegame" MainWindow
+Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: -1
+	at GameField.initGame(GameField.java:40)
+	at GameField.<init>(GameField.java:32)
+	at MainWindow.<init>(MainWindow.java:10)
+	at MainWindow.main(MainWindow.java:15)
+
+Process finished with exit code 1
+
+ */
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow(){
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,345);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        MainWindow mw = new MainWindow();
+    }
+}
